976216028f43f43e6dd10f54ba609d885531dae1
Fragile-azalea <1872891695@qq.com>
2017-08-19 21:49:59 +0800

    Update gradientDescentMulti.m

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -17,11 +17,8 @@ for iter = 1:num_iters
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
-
-
-
-
-
+    tem = alpha / m * X' * (X * theta - y);
+    theta = theta - tem;
 
 
 
